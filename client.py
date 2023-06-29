4cda80e468c0059aa80e01997186a265744ca8f3
Murilo <56661791+Murilomsq@users.noreply.github.com>
2023-06-28 21:08:29 -0300

    Update client.py

--- client.py
+++ client.py
@@ -4,10 +4,10 @@ from constRPYC import * #-
 class Client:
   conn = rpyc.connect(SERVER, PORT) # Connect to the server
   print (conn.root.exposed_value())
-  conn.root.exposed_append(5)       # Call an exposed operation,
-  conn.root.exposed_append(6)       # and append two elements
-  conn.root.exposed_search(6)
-  conn.root.exposed_search(4)
-  conn.root.exposed_remove(6)
-  conn.root.exposed_sort()
+  print (conn.root.exposed_append(5))       # Call an exposed operation,
+  print(conn.root.exposed_append(6))       # and append two elements
+  print(conn.root.exposed_search(6))
+  print(conn.root.exposed_search(4))
+  print(conn.root.exposed_remove(6))
+  print(conn.root.exposed_sort())
   print (conn.root.exposed_value())   # Print the result
