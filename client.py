286bc5006c17df3af810fe62328d8fcbba3efe0b
Murilo <56661791+Murilomsq@users.noreply.github.com>
2023-06-28 21:05:57 -0300

    Update client.py

--- client.py
+++ client.py
@@ -7,4 +7,7 @@ class Client:
   conn.root.exposed_append(5)       # Call an exposed operation,
   conn.root.exposed_append(6)       # and append two elements
   conn.root.exposed_search(6)
+  conn.root.exposed_search(4)
+  conn.root.exposed_remove(6)
+  conn.root.exposed_sort()
   print (conn.root.exposed_value())   # Print the result
